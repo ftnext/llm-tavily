8e12bbc0eed8a1a08c2a6ac209e822d8a2c9df1b
ftnext <takuyafjp+develop@gmail.com>
2025-07-13 23:38:53 +0900

    [feat] llm -m qa-tavily 'prompt (question)'

--- llm_tavily.py
+++ llm_tavily.py
@@ -1,11 +1,18 @@
-import click
 import llm
+from tavily import TavilyClient
+
+
+class TavilyQnASearch(llm.KeyModel):
+    model_id = "qa-tavily"
+    needs_key = "tavily"
+    key_env_var = "LLM_TAVILY_KEY"
+
+    def execute(self, prompt, stream, response, conversation, key):
+        tavily_client = TavilyClient(api_key=key)
+        answer = tavily_client.qna_search(prompt.prompt)
+        return answer
 
 
 @llm.hookimpl
-def register_commands(cli):
-    return
-    # @cli.command()
-    # def hello_world():
-    #     "Say hello world"
-    #     click.echo("Hello world!")
+def register_models(register):
+    register(TavilyQnASearch())
